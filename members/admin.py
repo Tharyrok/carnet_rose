a6f5c0abe7ca9fc5758298ee82fb53568dca299b
Laurent Peuch <cortex@worlddomination.be>
2013-11-23 00:13:57 +0100

    [enh] better display of the member form

--- members/admin.py
+++ members/admin.py
@@ -1,10 +1,27 @@
+# encoding: utf-8
+
 import reversion
 from django.contrib import admin
 from .models import Member
 
 
 class MemberAdmin(reversion.VersionAdmin):
-    list_display = ('first_name', 'last_name', 'last_paid_date')
+    list_display = ('first_name', 'last_name', 'last_paid_date', 'ca_member')
+    fieldsets = (
+        ('Information générales', {
+            'fields': (('first_name', 'last_name', 'juridical_form'), ('address', 'email')),
+        }),
+        ('Cotisation', {
+            'fields': (('member_since', 'last_paid_date'),),
+        }),
+        ('Conseil d\'administration', {
+            'fields': ('ca_member', 'ca_function'),
+        }),
+        ('Résignation', {
+            'fields': ('member_end', 'why_member_end'),
+            'classes': ('collapse',),
+        }),
+    )
 
 
 admin.site.register(Member, MemberAdmin)
