d23dc6d6c3f68d017b93e207ab306e8e432ac595
Laurent Peuch <cortex@worlddomination.be>
2013-11-23 00:29:41 +0100

    [enh] activate search

--- members/admin.py
+++ members/admin.py
@@ -27,6 +27,7 @@ class MemberAdmin(reversion.VersionAdmin):
     list_display = ('first_name', 'last_name', 'last_paid_date', 'ca_member')
     list_filter = ('last_paid_date', 'ca_member', IsStillMember)
     radio_fields = {"juridical_form": admin.HORIZONTAL}
+    search_fields = ('first_name', 'last_name')
     fieldsets = (
         ('Information générales', {
             'fields': (('first_name', 'last_name',), ('address', 'email'), 'juridical_form'),
