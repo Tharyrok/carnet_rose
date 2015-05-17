4adf75278ebe766b65c65e4335a4bb3441f9d6a9
Laurent Peuch <cortex@worlddomination.be>
2015-05-17 16:46:18 +0200

    [enh] add subscription related field to member listing

--- members/admin.py
+++ members/admin.py
@@ -24,7 +24,7 @@ class IsStillMember(admin.SimpleListFilter):
 
 
 class MemberAdmin(reversion.VersionAdmin):
-    list_display = ('first_name', 'last_name', 'last_paid_date', 'ca_member')
+    list_display = ('first_name', 'last_name', 'last_paid_date', 'ca_member', 'vpn', 'cube')
     list_filter = ('last_paid_date', 'ca_member', IsStillMember)
     radio_fields = {"juridical_form": admin.HORIZONTAL}
     search_fields = ('first_name', 'last_name')
