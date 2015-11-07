36672ca7b3bfbf6c23645d7510783923be88b284
Laurent Peuch <cortex@worlddomination.be>
2015-11-07 22:37:18 +0100

    [enh] new empty view for uploading recordbank csv

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,10 @@ from django.views.generic import ListView
 
 from .models import Movement
 
+from . import views
 
-urlpatterns = patterns('accounts.views',
+
+urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Movement, template_name="accounts/home.haml"), name='accounts_home'),
+    url(r'^upload_record_bank_csv/$', views.upload_record_bank_csv, name='accounts_upload_record_bank_csv'),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def upload_record_bank_csv(request):
+    pass
