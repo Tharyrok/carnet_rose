10404c4c3e7e6a4582bc6d6255ee7bffaf6ea682
Laurent Peuch <cortex@worlddomination.be>
2015-11-08 00:50:25 +0100

    [enh] skip already handled movements

--- accounts/banks.py
+++ accounts/banks.py
@@ -1,6 +1,10 @@
 import csv
 from StringIO import StringIO
 
+from .models import Movement
+
 def handle_recordbank_csv(csv_file):
     for entry in csv.DictReader(StringIO("\r\n".join(csv_file.read().split("\n")[1:]) + "\r\n"), delimiter=";"):
-        break
+        # I've already imported this movement, don't do anything
+        if Movement.objects.filter(bank_id=entry["Ref. v/d verrichting"]):
+            continue
