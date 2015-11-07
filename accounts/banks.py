1ebbc19662fe56c643c071483c900d49d1d2ce7a
Laurent Peuch <cortex@worlddomination.be>
2015-11-08 00:52:50 +0100

    [fix] more djangonic

--- accounts/banks.py
+++ accounts/banks.py
@@ -7,7 +7,7 @@ from .models import Movement
 def handle_recordbank_csv(csv_file):
     for entry in csv.DictReader(StringIO("\r\n".join(csv_file.read().split("\n")[1:]) + "\r\n"), delimiter=";"):
         # I've already imported this movement, don't do anything
-        if Movement.objects.filter(bank_id=entry["Ref. v/d verrichting"]):
+        if Movement.objects.filter(bank_id=entry["Ref. v/d verrichting"]).exists():
             continue
 
         movement = Movement()
