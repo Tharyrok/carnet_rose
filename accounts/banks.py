86154fed906d5169b66d9705e9e330ea1bda5e78
Laurent Peuch <cortex@worlddomination.be>
2015-11-08 00:51:54 +0100

    [enh] start to handle movements from csv

--- accounts/banks.py
+++ accounts/banks.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 from StringIO import StringIO
 
 from .models import Movement
@@ -8,3 +9,24 @@ def handle_recordbank_csv(csv_file):
         # I've already imported this movement, don't do anything
         if Movement.objects.filter(bank_id=entry["Ref. v/d verrichting"]):
             continue
+
+        movement = Movement()
+        movement.bank_id = entry["Ref. v/d verrichting"]
+        movement.date = datetime.strptime(entry["Datum v. verrichting"], "%d-%m-%Y").date()
+        movement.amount = float(entry["Bedrag v/d verrichting"].replace(".", "").replace(",", "."))
+
+        if movement.amount < 0:
+            movement.kind = "debit"
+
+            # XXX could we have float errors here?
+            # I might want to do string parsing to remove the "-"
+            # but that sucks
+            movement.amount = -1 * movement.amount
+        else:
+            movement.kind = "credit"
+
+        movement.comment = "From: %s\nCommunition: '%s'" % (entry["Naam v/d tegenpartij :"], entry["Mededeling 1 :"])
+
+        movement.title = "FIXME"
+
+        print movement
