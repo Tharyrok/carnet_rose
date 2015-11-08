783f49ffaeb330786531624ffeac75e9f1eeb777
Laurent Peuch <cortex@worlddomination.be>
2015-11-08 03:13:13 +0100

    [enh] page to list all import reports

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Movement, template_name="accounts/home.haml"), name='accounts_home'),
+    url(r'^import_report/$', ListView.as_view(model=ImportReport, template_name="accounts/importreport_list.haml"), name='accounts_importreport_list'),
     url(r'^import_report/(?P<pk>\d+)/$', DetailView.as_view(model=ImportReport, template_name="accounts/importreport_detail.haml"), name='accounts_importreport_detail'),
     url(r'^upload_record_bank_csv/$', views.UploadRecordBankCsv.as_view(), name='accounts_upload_record_bank_csv'),
 )
